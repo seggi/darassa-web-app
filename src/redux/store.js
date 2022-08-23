ec5f47af19cb853702dae7eb35efb7289c8d2e7d
Seggi SMS <seggimarugira@gmail.com>
2022-08-23 12:34:41 +0200

    configure redux saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,17 @@
-import { createStore } from "redux";
-import allReducers from "./reducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import rootReducer from "./reducers";
+import rootSaga from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 
-store.getState();
+sagaMiddleware.run(rootSaga);
+
 export default store;
