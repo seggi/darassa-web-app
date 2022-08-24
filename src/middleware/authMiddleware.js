6c3b48c3b1794494b646f3bfe5d6f310834f6425
Seggi SMS <seggimarugira@gmail.com>
2022-08-24 10:17:01 +0200

    setup route

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,36 +1,13 @@
 import { React } from "react";
 import PropTypes from "prop-types";
-import { Route, Navigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 
-const AuthMiddleware = ({
-  element: Element,
-  layout: Layout,
-  isAuthProtected,
-  ...rest
-}) => (
-  <Route
-    render={(props) => {
-      if (isAuthProtected && !localStorage.getItem("authUser")) {
-        return (
-          <Navigate
-            to={{ pathname: "/login", state: { from: props.location } }}
-          />
-        );
-      }
-      return (
-        // <Layout>
-        <Element {...props} />
-        // </Layout>
-      );
-    }}
-  />
-);
+const AuthMiddleware = ({ isAuthProtected }) => {
+  return isAuthProtected === true ? <Outlet /> : <Navigate to={"/login"} />;
+};
 
 AuthMiddleware.prototypes = {
   isAuthProtected: PropTypes.bool,
-  component: PropTypes.any,
-  location: PropTypes.object,
-  layout: PropTypes.any,
 };
 
 export default AuthMiddleware;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,25 @@
-/* eslint-disable import/no-anonymous-default-export */
-import login from "./login";
+import LoginContainer from "../containers/auth/LoginContainer";
+import DashboardContainer from "../containers/dashboard";
 
-export default [login];
+const routes = [
+  {
+    path: "/login",
+    element: <LoginContainer />,
+    isProtected: false,
+    key: "login",
+  },
+  {
+    path: "/",
+    element: <DashboardContainer />,
+    isProtected: true,
+    key: "dashboard",
+  },
+  {
+    path: "/404",
+    element: <h1>404 Page Error</h1>,
+    isProtected: true,
+    key: "dashboard",
+  },
+];
+
+export default routes;
